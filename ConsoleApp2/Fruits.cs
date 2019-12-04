8750546a017bb94a570e0260689c3057a0be1a3c
slavawarik <slavawarik@gmail.com>
2019-12-04 16:56:56 +0500

    add project

---
+++ ConsoleApp2/Fruits.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+public class Fruits
+{ 
+       
+       
+              public string name;
+              public string color;
+              public string storage_life;
+
+              public void Info()
+              {
+              Console.WriteLine($"Название: {this.name} Цвет: {color} Срок годности: {storage_life}");
+              }
+              public Fruits() : this("Название")
+              {
+
+              }
+              public Fruits(string name) : this(name, "Красный", "4 месяца")
+              {
+
+              }
+              public Fruits(string name, string storage_life) : this(name, "Красный", storage_life)
+              {
+
+              }
+              public Fruits(string name, string color, string storage_life)
+              {
+              this.name = name;
+              this.color = color;
+              this.storage_life = storage_life;
+
+              }
+
+
+       
+   
+}
+
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Fruits kiwi = new Fruits("Киви", "Зеленое", "3 месяца");
+            kiwi.Info();
+            Fruits apple = new Fruits("Яблоко", "Красное", "4 месяца");
+            apple.Info();
+            Fruits banana = new Fruits("Банан", "Желтый","2 месяца");
+            banana.Info();
+            Fruits orange = new Fruits("Апельсин","Оранжевый","5 месяцов");
+            orange.Info();
+            Fruits lemon = new Fruits("Лемон", "Желтый", "1 месяц");
+            lemon.Info();
+            Console.ReadLine();
+        
+        }
+        
+    }
+}
